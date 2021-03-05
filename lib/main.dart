ce0a2e6a643e5c5bdc39662da71fc4d325ccc03b
aizackSF <70985216+aizackSF@users.noreply.github.com>
2021-03-05 16:46:36 -0300

    Updated Transaction Card component

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 // Models
 import './models/transactions.dart';
@@ -35,7 +36,43 @@ class MyHomePage extends StatelessWidget {
               child: Text('Gráfico'),
             ),
           ),
-          Card(child: Text('Lista de Transações')),
+
+          Column(
+            children: _transactions.map((transaction) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      padding: EdgeInsets.all(10),
+
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.deepPurple,
+
+                          width: 2,
+                        )
+                      ),
+
+                      child: Text(
+                        'R\$ ${transaction.value.toStringAsFixed(2)}',
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.deepPurple)
+                      ),
+                    ),
+
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      children: [
+                        Text(transaction.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                        Text(DateFormat('d MMM y').format(transaction.date), style: TextStyle(color: Colors.grey)),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          )
         ],
       ),
     );
